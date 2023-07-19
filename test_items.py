303569f27beeec52c610a2870c75ab57edbdb5dc
Elena <peregovorun@gmail.com>
2023-07-20 01:33:36 +0300

    update test_items.py

--- test_items.py
+++ test_items.py
@@ -8,5 +8,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_button_add_to_cart_on_page(browser):
     browser.get(link)
     time.sleep(10)
-    button = browser.find_element(By.CLASS_NAME, "btn-primary")
+    button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert button
